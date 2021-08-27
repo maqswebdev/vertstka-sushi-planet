f37e39bd7018b6efe79e1b7e48b59b75967a8440
jokeejoke <69756862+jokeejoke@users.noreply.github.com>
2021-08-27 18:47:18 +0600

    adaptiroval top header

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,13 @@
 //import $ from 'jquery'; window.jQuery = $; window.$ = $ // import module example (npm i -D jquery)
 
-
-document.addEventListener('DOMContentLoaded', () => {
-
-	// Your JS Code
-
-})
+document.addEventListener("DOMContentLoaded", () => {
+  // Hamburger Menu
+  const $hamburgerBtn = document.querySelector(".hamburger");
+  const $overlayMnu = document.querySelector(".overlay-nav");
+  if ($hamburgerBtn) {
+    $hamburgerBtn.addEventListener("click", function () {
+      this.classList.toggle("is-active");
+      $overlayMnu.classList.toggle("active");
+    });
+  }
+});
