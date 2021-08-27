aa37f32567d585b1ce20c7452050074e1638168b
jokeejoke <69756862+jokeejoke@users.noreply.github.com>
2021-08-27 12:39:29 +0600

    add gulpv4 add favicon

---
+++ app/js/app.js
@@ -0,0 +1,8 @@
+//import $ from 'jquery'; window.jQuery = $; window.$ = $ // import module example (npm i -D jquery)
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+	// Your JS Code
+
+})
