4c53f34b687d5454823860015bee46294a674f02
jokeejoke <69756862+jokeejoke@users.noreply.github.com>
2021-09-05 17:00:36 +0600

    fixed fixed-header and add cart page

--- app/js/app.js
+++ app/js/app.js
@@ -9,63 +9,54 @@ import mPageScroll2id from "page-scroll-to-id";
 
 document.addEventListener("DOMContentLoaded", () => {
   // Hamburger Menu
-  const $hamburgerBtn = document.querySelector(".hamburger");
-  const $overlayMnu = document.querySelector(".overlay-nav");
-  if ($hamburgerBtn) {
-    $hamburgerBtn.addEventListener("click", function () {
-      if (this.classList.contains("is-active")) {
-        this.classList.remove("is-active");
-        $overlayMnu.classList.remove("active");
-        document.body.style.overflow = "";
-      } else {
-        this.classList.add("is-active");
-        $overlayMnu.classList.add("active");
-        document.body.style.overflow = "hidden";
+  setTimeout(() => {
+    const $hamburgerBtns = document.querySelectorAll(".hamburger");
+    const $overlayMnu = document.querySelector(".overlay-nav");
+    $hamburgerBtns.forEach((burgerBtn) => {
+      if (burgerBtn) {
+        burgerBtn.addEventListener("click", function () {
+          if (this.classList.contains("is-active")) {
+            this.classList.remove("is-active");
+            $overlayMnu.classList.remove("active");
+            document.body.style.overflow = "";
+          } else {
+            this.classList.add("is-active");
+            $overlayMnu.classList.add("active");
+            document.body.style.overflow = "hidden";
+          }
+        });
       }
     });
-  }
+  }, 500);
+
   // Create a new instance of Headhesive || Липкое меню
   const header = new Headhesive(".header", {
     offset: 120,
-    classes: {},
   });
 
-  // SWIPER SLIDERS INIT
-  const $mobileMenu = document.querySelector(".mobile-menu");
-  function initMobileMenu() {
-    const mobileMenuSlider = new Swiper(".mobile-menu", {
-      slidesPerView: "auto",
-      spaceBetween: window.innerWidth <= 767 ? 8 : 10,
-      centeredSlides: true,
-      loop: true,
-    });
-    const $mobileMenuWrapper = document.querySelector(".mobile-menu__wrapper");
-    if ($mobileMenuWrapper) {
-      setTimeout(() => {
-        $mobileMenuWrapper.style.transform = "translate3d(0, 0, 0)";
-        $mobileMenuWrapper.style["-webkit-transform"] = "translate3d(0, 0, 0)";
-      }, 2000);
-    }
-  }
-  // END SWIPER SLIDERS INIT
-
-  /** START FUNCTIONS ON TABLET AND MOBILE VERSION */
-  /*if (window.innerWidth <= 991) {
-    initMobileMenu();
-  }
-  window.addEventListener(
-    "resize",
-    function () {
-      if (
-        !$mobileMenu.classList.contains("swiper-container-initialized") &&
-        window.innerWidth <= 991
-      ) {
-        console.log("init");
-        initMobileMenu();
-      }
+  const additionListSlider = new Swiper(".addition-slider", {
+    slidesPerView: 2,
+    spaceBetween: 10,
+    freeMode: true,
+    breakpoints: {
+      767: {
+        slidesPerView: 4,
+        spaceBetween: 15,
+      },
+      991: {
+        slidesPerView: 5,
+      },
     },
-    false
-  );*/
+  });
+
+  const $cartHeaderBackBtn = document.querySelectorAll(
+    ".cart-header .back-btn span"
+  );
+  $cartHeaderBackBtn.forEach((cartHeaderItem) => {
+    window.innerWidth <= 1200
+      ? (cartHeaderItem.innerHTML = "Back")
+      : (cartHeaderItem.innerHTML = "Back to menu");
+  });
 
   /* JQUERY FUNCTIONS !!!!!!!!! */
 
